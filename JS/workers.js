853a167393e946be0fda42d9e0a0ea4f76afa37d
Gamzat <104684754+RGamz@users.noreply.github.com>
2023-03-10 13:34:07 +0100

    workers

---
+++ JS/workers.js
@@ -0,0 +1,20 @@
+const workers = [
+    {"name":"John","salary":500},
+    {"name":"Mike","salary":1300},
+    {"name":"Linda","salary":1500}];
+
+arrResult = []
+
+let getWorthyWorkers = function(array) {
+    //принимаем на вход массив, прогоняем объекты по salary, и если они больше 1000, выводим name. 
+
+    for (var i = 0; i<workers.length;i++) {
+
+        if (workers[i].salary > 1000) {
+            arrResult.push(workers[i].name)
+        }
+    }
+    return arrResult
+}
+
+console.log(getWorthyWorkers(workers))
