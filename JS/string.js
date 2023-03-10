e4b2a3e5cf609ebf51c0a69a3a60b0b377a1d7b9
Gamzat <104684754+RGamz@users.noreply.github.com>
2023-03-10 13:49:27 +0100

    added string js file

---
+++ JS/string.js
@@ -0,0 +1,11 @@
+const path = "/users/download/index.html"
+
+const isHtml = function(x){
+    if (path.slice(path.length-5, path.length) == ".html") {
+        return "true"
+    } else {
+        return "false"
+    }
+}
+
+console.log(isHtml(path))
